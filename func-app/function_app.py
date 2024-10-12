eeda970c3201a576ebb77afa7095ce370db7618b
Prateek Jain <prateejain@microsoft.com>
2024-10-12 14:55:28 -0700

    Added the function for query & summarization

--- func-app/function_app.py
+++ func-app/function_app.py
@@ -22,6 +22,43 @@ def summarize_query(req: func.HttpRequest) -> func.HttpResponse:
         status_code=200
     )
 
+@app.function_name('csquery')
+@app.route(route="query", auth_level=func.AuthLevel.FUNCTION)
+def query(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Parameters1: "+str(req.params))
+    if executing_correct_func_app(req, "csquery"):
+        return func.HttpResponse(
+        "Please trigger csquery Azure function for query",
+        status_code=200
+        )
+    if 'context_id' in req.params:
+        context_id=req.params['context_id']
+        query=req.params['query']
+        path=int(req.params['path'])
+    else:
+        return func.HttpResponse(
+        "Must send context id and context operation",
+        status_code=200
+        )
+    logging.info("Query start")
+    output = run_local_search(
+                None,
+                None,
+                "settings",
+                community_level=2,
+                response_type="",
+                context_id=context_id,
+                query=query,
+                optimized_search=False,
+                use_kusto_community_reports=False,
+                path=path
+            )
+
+    return func.HttpResponse(
+        "Query completed: "+ output,
+        status_code=200
+    )
 
 def executing_correct_func_app(req: func.HttpRequest, route: str):
     return os.getenv("ENVIRONMENT") == "AZURE" and  os.getenv("APP_NAME")!= route
