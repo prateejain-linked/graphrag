4766a4072b4a2b0e2c894a88a5411bc6939011e5
Prateek Jain <prateejain@microsoft.com>
2024-10-12 13:37:48 -0700

    Removing the second indexer and reducing the freq of first

--- func-app/function_app.py
+++ func-app/function_app.py
@@ -40,7 +40,7 @@ def initialize_watermark_client() -> BlobPipelineStorage:
     
 
 @app.function_name('csindexer')
-@app.timer_trigger(schedule="0 */3 * * * *", arg_name="mytimer", run_on_startup=True) 
+@app.timer_trigger(schedule="0 */1 * * * *", arg_name="mytimer", run_on_startup=True) 
 def indexing(mytimer: func.TimerRequest) -> None:
     logging.info('Python HTTP trigger function processed a request.')
     
