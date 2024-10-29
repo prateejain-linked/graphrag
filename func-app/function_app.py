6a9a7c94e9414dc747cdb8bc8173c9b35c5f504d
Liam Dannaher <liamdannaher@microsoft.com>
2024-10-29 14:25:09 -0700

    Adding blueprint to context_switch

--- func-app/function_app.py
+++ func-app/function_app.py
@@ -1,16 +1,9 @@
-import azure.functions as func
-import datetime
-import json
-import logging
-import csv
-import codecs
-from graphrag.index.cli import index_cli
 import os
 
-from graphrag.query.cli import run_local_search, summarize,rrf_scoring
-from time import sleep
-from query_save import query_functions
+import azure.functions as func
+from context_switch_function import context_functions
 from index import index_functions
+from query_save import query_functions
 
 app = func.FunctionApp()
 
@@ -18,7 +11,9 @@ function_type = os.environ.get("FUNCTIONTYPE", default="indexing")
 if(function_type == "query"):
     app.register_functions(query_functions)
 
-if(function_type == "indexing"):
+elif(function_type == "indexing"):
     app.register_functions(index_functions)
 
+elif(function_type == "context"):
+    app.register_functions(context_functions)
 
