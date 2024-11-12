2a4bb5a1b8fb3c1d0f9ba6bddaf59d3eed0e5b78
Guillermo Salvador Barrón Sánchez <gbarrnsnchez@microsoft.com>
2024-11-12 10:11:29 -0800

    Local changes for playbook

--- func-app/query_save.py
+++ func-app/query_save.py
@@ -10,6 +10,9 @@ import os
 from graphrag.query.cli import run_local_search, summarize,rrf_scoring,expand_node_graph
 from time import sleep
 
+from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
+
 query_functions = func.Blueprint()
 
 @query_functions.function_name('query')
@@ -79,7 +82,8 @@ def query_save(req: func.HttpRequest) -> func.HttpResponse:
 
     return func.HttpResponse(
         json.dumps(json_res),
-        status_code=200
+        status_code=200,
+        headers={'Access-Control-Allow-Origin':"*",'Access-Control-Allow-Methods':'POST','Access-Control-Allow-Headers':'Content-Type, Authorization'}
     )
 
 @query_functions.function_name('summarization')
@@ -89,8 +93,9 @@ def summarize_query(req: func.HttpRequest) -> func.HttpResponse:
     query_id = req.params['query_id']
     output = summarize(query_id=query_id, root_dir='settings')
     return func.HttpResponse(
-        output,
-        status_code=200
+        json.dumps(output),
+        status_code=200,
+        headers={'Access-Control-Allow-Origin':"*",'Access-Control-Allow-Methods':'POST','Access-Control-Allow-Headers':'Content-Type, Authorization'}
     )
 
 
@@ -103,7 +108,8 @@ def rrf(req: func.HttpRequest) -> func.HttpResponse:
     json_res={'query_id':output}
     return func.HttpResponse(
         json.dumps(json_res),
-        status_code=200
+        status_code=200,
+        headers={'Access-Control-Allow-Origin':"*",'Access-Control-Allow-Methods':'POST','Access-Control-Allow-Headers':'Content-Type, Authorization'}
     )
 
 
@@ -120,3 +126,78 @@ def query_expansion(req: func.HttpRequest) -> func.HttpResponse:
         str(output),
         status_code=200
     )
+
+
+@query_functions.route(route="contexts")
+def http_context(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # Azure Blob Storage connection details
+    # connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+    storage_account_blob_url = "https://inputdatasetsa.blob.core.windows.net"
+    container_name = "context"
+    blob_name = req.params.get('blob_name')
+
+    try:
+        # Create the BlobServiceClient object
+        if 'AZURE_CLIENT_ID' in os.environ:
+            print("AZURE_CLIENT_ID: ", os.environ['AZURE_CLIENT_ID'])
+
+        # credential = DefaultAzureCredential()
+        credential = DefaultAzureCredential(managed_identity_client_id="500051c4-c242-4018-9ae4-fb983cfebefd", exclude_interactive_browser_credential = False)
+        print("DefaultAzureCredential: ", credential)
+        sleep(1)
+        blob_service_client = BlobServiceClient(
+                account_url=storage_account_blob_url,
+                credential=credential,
+            )
+        print("Successfully connected to Blob Storage")
+
+        # Get the container client
+        container_client = blob_service_client.get_container_client(container_name)
+
+        # Get the blob client
+        if blob_name is not None:
+            blob_client = container_client.get_blob_client(blob_name)
+            blob_data = blob_client.download_blob().readall()
+        else:
+            # Return a list of all blobs in the container
+            print("FAILED HERE0!!!!!")
+            blobs = container_client.list_blobs()
+            # Convert the blobs to a list
+            print("FAILED HERE1!!!!!")
+            print("BLOBS"+str(blobs))
+            blob_test = [blob for blob in blobs]
+            print("BLOB TEST"+str(blob_test))
+            blob_data = [blob.name for blob in blobs]
+            # Filter to just get the files which follow the pattern *_init.json
+            print("FAILED HERE2!!!!!")
+            blob_data = [blob for blob in blob_data if blob.endswith("_init.json")]
+            # Read each blob content and return a list of all the jsons
+            print("FAILED HERE3!!!!!")
+            blob_data = [container_client.get_blob_client(blob).download_blob().readall().decode('utf-8') for blob in blob_data]
+            # Test only returning an array of the first blob
+            # blob_data = blob_data[0]
+            # as an array
+            # blob_data = [blob_data]
+
+            print(blob_data)
+            return func.HttpResponse(
+                body=json.dumps(blob_data),
+                status_code=200,
+                mimetype="application/json"
+            )
+
+        # Download the blob content
+
+        return func.HttpResponse(
+            body=blob_data,
+            status_code=200,
+            mimetype="application/octet-stream"
+        )
+    except Exception as e:
+        logging.error(f"Error querying Blob Storage: {e}")
+        return func.HttpResponse(
+            body="Error querying Blob Storage",
+            status_code=500
+        )
