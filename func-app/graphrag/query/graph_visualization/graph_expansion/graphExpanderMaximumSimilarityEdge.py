14687d5f2c2146dccb9157bab95830ca207a3fa0
Guillermo Salvador Barrón Sánchez <gbarrnsnchez@microsoft.com>
2024-11-14 16:44:43 -0800

    Add description and missing config parameter

--- func-app/graphrag/query/graph_visualization/graph_expansion/graphExpanderMaximumSimilarityEdge.py
+++ func-app/graphrag/query/graph_visualization/graph_expansion/graphExpanderMaximumSimilarityEdge.py
@@ -26,6 +26,7 @@ class GraphExpanderMaximumSimilarityEdge(BaseGraphExpander):
                     top_k_path_edge_source,
                     top_k_path_edge_target,
                     text_unit = top_k_path_edge[2][0],
+                    description = "Edge from expansion"
                 )
         else:
             all_edges = self.get_all_edges_subtree(node,depth)
@@ -43,6 +44,7 @@ class GraphExpanderMaximumSimilarityEdge(BaseGraphExpander):
                     top_k_edge_source,
                     top_k_edge_target,
                     text_unit = top_k_edge.text_unit_ids[0],
+                    description = "Edge from expansion"
                 )
         graphml = "".join(nx.generate_graphml(graph,named_key_ids=True))
         #return graphml.replace("<graphml",f"<graphml initial_node={node}")
